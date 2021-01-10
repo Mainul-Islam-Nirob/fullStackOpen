e04956b9e3b75c95b30d8aa87c7cee396f60c7d8
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-01-10 22:24:40 +0600

    unicafe step4 solution added

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,36 +3,47 @@ import ReactDOM from 'react-dom'
 
 const Statistics = (props) => {
   console.log("Statistics", props)
-  const {good, neutral, bad, all, average, positive} = props
+  const {good, neutral, bad, total, average, positive} = props
 
+  if(total === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given!!</p>
+      </div>
+    )
+    
+  }
   return (
     <div>
       <h1>Statistics</h1>
-      <p>Good {good}</p>    
-      <p>Neutral {neutral}</p> 
-      <p>Bad {bad}</p> 
-      <p>Total {all}</p>
-      <p>Average {average}</p> 
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>Total {total}</p>
+      <p>Average {average}</p>
       <p>Positive {positive} %</p>
     </div>
   )
 
 }
+ 
+    
 
-const App = () => {
+function App() {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const all = good + neutral + bad
   const average = (good * 1 + neutral * 0 + bad * -1) / all
-  const positive = (good / all) * 100
-  
-  const incGood =() => setGood(good + 1);
-  const incNeutral =() => setNeutral(neutral + 1)
-  const incBad = () => setBad(bad + 1)
-    
+  const positive = (good / all) * 100;
+
+  const incGood = () => setGood(good + 1);
+  const incNeutral = () => setNeutral(neutral + 1);
+  const incBad = () => setBad(bad + 1);
+
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -42,7 +53,7 @@ const App = () => {
 
       <Statistics good={good} neutral={neutral} bad={bad} total={all} average={average} positive={positive} />
     </div>
-  )
+  );
 }
 
 ReactDOM.render(<App />,
