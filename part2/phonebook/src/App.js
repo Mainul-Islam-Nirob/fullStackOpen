db030c6ff480198b8c2dc9dec1a4d953d86082f5
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-01-23 20:55:05 +0600

    2.9*: The Phonebook Step4 added

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
-import Person from "./Components/Person"
+import Persons from "./Components/Persons"
 
 const App = () => {
-    const [persons, setPersons] = useState([])
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    ])
     const [newName, setNewName] = useState("")
     const [newNumber, setNewNumber] = useState("")
+    const [filter, setFilter] = useState("")
+    const [filteredPersons, setFilteredPersons] = useState(null)
 
    // Submit Form
     const addPerson = (event) => {
@@ -39,10 +46,29 @@ const App = () => {
     const handleNumberChange = (event) => {
         setNewNumber(event.target.value)
     }
+    
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value)
+        
+        const filtered = persons.filter(person =>
+             // Check if the search term is exist in the names of phonebook
+             person.name.toLowerCase().includes(event.target.value.toLowerCase())
+             )
+             
+             setFilteredPersons(filtered);
+    }
 
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                filter shown with 
+                <input 
+                 value={filter}
+                 onChange={handleFilterChange} />
+            </div>
+
+            <h2>add a new</h2>
             <form onSubmit={addPerson}>
                 <div>
                     name: <input
@@ -60,10 +86,11 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(person => 
-                   <Person key={person.name} person={person.name} number={person.number} />
-
-                )}
+            <Persons
+                filter={filter}
+                persons={persons}
+                filteredPersons={filteredPersons}
+            />
         </div>
     )
 }
--- part2/phonebook/src/Components/Person.js
+++ part2/phonebook/src/Components/Person.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Person = ({person, number}) => {
-    return (
-    <p>{person} {number}</p>
-    
+const Person = ({person}) => (
+    <div>
+        <span>{person.name} : </span>
+        <span>{person.number}</span>
+    </div>  
     )
-}
 
 export default Person
