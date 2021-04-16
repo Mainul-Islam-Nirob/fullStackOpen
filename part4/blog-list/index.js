e42f1d95136a3d8f930e54199eb81febdb9ac361
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-04-16 19:41:57 +0600

    4.2 Blog list, step2, Final commit of Refactoring blog-list app and added app.js file

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,36 +1,11 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const http = require('http')
 const config = require('./utils/config')
+
 const logger = require('./utils/logger')
-const blogsRouter = require("./controllers/blogs")
-const middleware = require("./utils/middleware")
+const server = http.createServer(app)
 
-mongoose
-    .connect(config.MONGODB_URI, { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true, 
-        useFindAndModify: false, 
-        useCreateIndex: true })
-    .then(() => {
-        logger.info("connected to MongoDB")
-    })
-    .catch((err) => {
-        logger.error("Error connecting to db", err.message)
-    })
-
-
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use("/api/blogs", blogsRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-
-app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`)
+const PORT = config.PORT
+server.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`)
 })
