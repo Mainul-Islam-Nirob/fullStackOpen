10439d53ac7676ae60ad7576ead4cb099ec77010
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-04-16 19:24:59 +0600

    added custom middleware into utils/middleware.js

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require("./controllers/blogs")
+const middleware = require("./utils/middleware")
 
 mongoose
     .connect(config.MONGODB_URI, { 
@@ -22,7 +23,12 @@ mongoose
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use("/api/blogs", blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 
 app.listen(config.PORT, () => {
