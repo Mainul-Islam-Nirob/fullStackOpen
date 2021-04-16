a196175921bffac4a9f88f2e499ea6b5ab774713
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-04-16 16:22:31 +0600

    handling of environment variables is extracted into a separate utils/config.js file

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,12 +1,11 @@
-// think
 require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-// think
 const blogSchema = new mongoose.Schema({
     title: String,
     author: String,
@@ -16,10 +15,8 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const url = process.env.MONGODB_URI
-
 mongoose
-    .connect(url, { 
+    .connect(config.MONGODB_URI, { 
         useNewUrlParser: true, 
         useUnifiedTopology: true, 
         useFindAndModify: false, 
@@ -55,7 +52,7 @@ app.post('/api/blogs', (request, response) => {
         })
 })
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`)
+
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
