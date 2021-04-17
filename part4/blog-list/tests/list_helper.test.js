d923b2e89b4a324293a66c8c375784e39f516924
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-04-17 15:57:13 +0600

    4.5*: helper functions and unit tests, step3

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -89,5 +89,30 @@ describe('total likes', () => {
         expect(result).toBe(36)
     })
 
+})
 
+describe('favorite blog', () => {
+
+    test('when list has no blog equals null', () => {
+        const result = listHelper.favoriteBlog(noBlogs)
+        expect(result).toBe(null)
+    })
+
+    test('when list has only one blog equals the likes of that', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        expect(result).toEqual({
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            likes: 5,
+        })
+    })
+
+    test('when list has many blogs equals the most liked of that', () => {
+        const result = listHelper.favoriteBlog(blogs)
+        expect(result).toEqual({
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            likes: 12,
+        })
+    })
 })
--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -9,7 +9,28 @@ const totalLikes = (blogs) => {
     return total
 }
 
+const favoriteBlog = (blogs) => {
+    if (!blogs || blogs.length === 0) {
+        return null
+    }
+
+    // get most liked blogs
+    const max = blogs.reduce((prev, current) => {
+       return prev.likes > current.likes ? prev : current
+    })
+
+    // return most liked blog details
+    const favBlog = {
+        title: max.title,
+        author: max.author,
+        likes: max.likes,
+    }
+
+    return favBlog
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
 }
