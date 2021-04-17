fff7030a2c4d2bb40690aec1c9efa55d783a7af3
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-04-17 14:45:36 +0600

    4.4: helper functions and unit tests, step2

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
    return 1
 }
 
+const totalLikes = (blogs) => {
+    const total = blogs.reduce((total, currentValue)=> {
+        return total + currentValue.likes
+    }, 0)
+    return total
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
