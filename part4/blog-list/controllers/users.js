d4d5838fcf969aeb7b5027feddc79934c7667f3e
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-04-22 12:10:57 +0600

    4.16*: bloglist expansion, step4

--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -5,6 +5,16 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
   const body = request.body
 
+  if (!body.password || body.password === '') {
+    return response.status(400).json({ error: 'password is required' })
+  }
+
+  if (body.password && body.password.length < 4) {
+    return response
+      .status(400)
+      .json({ error: 'password should be at least 3 characters' })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
--- part4/blog-list/models/user.js
+++ part4/blog-list/models/user.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 3
+  },
   name: String,
-  passwordHash: String,
-  //   notes: [
-  //     {
-  //       type: mongoose.Schema.Types.ObjectId,
-  //       ref: 'Note'
-  //     }
-  //   ],
+  passwordHash: { type: String, required: true },
 })
 
 userSchema.set('toJSON', {
@@ -21,6 +21,8 @@ userSchema.set('toJSON', {
     delete returnedObject.passwordHash
   }
 })
+
+userSchema.plugin(uniqueValidator)
 
 const User = mongoose.model('User', userSchema)
 
