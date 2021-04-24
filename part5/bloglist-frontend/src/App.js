51f88d5b3659e860bc694b1a4ba73ea545bd3523
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-04-24 16:52:43 +0600

    5.2: bloglist frontend, step2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -16,6 +16,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -23,14 +31,21 @@ const App = () => {
     const user = await loginService.login({
       username, password,
     })
+
+    window.localStorage.setItem(
+      'loggedBlogappUser', JSON.stringify(user)
+    )
     setUser(user)
     setUsername('')
     setPassword('')
-    console.log('login successful', user)
     
     } catch (e) {
       console.log(e)
     }
+  }
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -63,6 +78,7 @@ const App = () => {
   const blogs_list = () => (
     <div>
       <h1>blogs</h1>
+      <button onClick={handleLogout} type="button">LogOut</button>
       <h2>{user.name} logged in</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
