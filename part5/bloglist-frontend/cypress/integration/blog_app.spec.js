b14fa4049a4438b2b1cb410943c77c2fe2b766c0
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-04-28 15:42:58 +0600

    5.18: bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -2,6 +2,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Nirob Chowdhury',
+      username: 'nirob',
+      password: 'nirob',
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -10,5 +16,25 @@ describe('Blog app', function () {
     cy.get("input[name='Username']")
     cy.get("input[name='Password']")
     cy.contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get("input[name='Username']").type('nirob')
+      cy.get("input[name='Password']").type('nirob')
+      cy.get('button[type="submit"]').click()
+      cy.contains('Nirob Chowdhury logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get("input[name='Username']").type('wrong')
+      cy.get("input[name='Password']").type('credentials')
+      cy.get('button[type="submit"]').click()
+      cy.contains('Wrong username or password')
+        .should('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html').should('not.contain', 'Nirob Chowdhur logged in')
+    })
   })
 })
