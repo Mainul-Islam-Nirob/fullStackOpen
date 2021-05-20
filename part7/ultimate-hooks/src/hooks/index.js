5ddeeb7037151049a767466d644a4107fe091d1b
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-05-20 23:45:45 +0600

    7.8: ultimate hooks

---
+++ part7/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,41 @@
+import {useState} from 'react'
+import axios from 'axios'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+
+export const useResource = (baseUrl) => {
+    const [resources, setResources] = useState([])
+
+    const getAll = () => {
+        const request = axios.get(baseUrl)
+        return request.then((response) => setResources(response.data))
+    }
+
+    const create = async (resource) => {
+        const response = await axios.post(baseUrl, resource)
+        setResources([...resources, response.data])
+    }
+
+    const service = {
+        create,
+        getAll
+    }
+
+    return [
+        resources, service
+    ]
+}
+
