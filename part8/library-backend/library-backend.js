4ecfd1d2574b4d711f007d27b2b59f06b6ba78c8
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-05-27 23:13:14 +0600

    8.5: Books by genre

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -101,7 +101,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book]!
+    allBooks(author: String, genre: String): [Book]!
     allAuthors: [Author!]!
   }
 `
@@ -111,12 +111,18 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (args.author) {
+      if (args.author && args.genre) {
+        return books.filter(
+          (book) =>
+            book.author === args.author && book.genres.includes(args.genre)
+        )
+      } else if (args.author) {
         return books.filter((book) => book.author === args.author)
-      }else{
-        return books
-      }
+      } else if (args.genre) {
+        return books.filter((book) => book.genres.includes(args.genre))
+      } else return books
     },
+    
     allAuthors: () => {
       const booksPerAuthor = authors.map((author) =>
         books.filter((book) => book.author === author.name)
