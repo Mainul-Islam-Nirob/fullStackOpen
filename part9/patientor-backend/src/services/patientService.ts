fcdfe79783f2a870a6078a1d062a835c37f082b1
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-06-11 00:20:23 +0600

    9.11: Patientor backend, step4

---
+++ part9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,23 @@
+import patientData from "../../data/patients";
+import { Patient, NonSensitivePatient } from "../types";
+
+const patients: Array<Patient> = patientData;
+
+const getPatients = (): Array<Patient> => {
+    return patients;
+};
+
+const getNonSensitivePatients = (): NonSensitivePatient[] => {
+    return patients.map(({ id, name, dateOfBirth, gender, occupation}) => ({
+        id,
+        name, 
+        dateOfBirth,
+        gender,
+        occupation
+    }));
+};
+
+export default {
+    getPatients,
+    getNonSensitivePatients
+};
