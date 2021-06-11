160eb6a0aafb50648d3289a3ab5c590cb21413a4
Mainul-Islam-Nirob <maainul.islam@gmail.com>
2021-06-11 15:19:59 +0600

    9.13: Patientor backend, step6

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from "express";
 import patientService from "../services/patientService";
+import toNewPatient from "../utils";
 
 const router = express.Router();
 
@@ -9,15 +10,26 @@ router.get("/", (_req, res) => {
 });
 
 router.post("/", (req, res) => {
-    const { name, dateOfBirth, gender, ssn, occupation } = req.body;
-    const newPatient = patientService.addPatient({
-        name,
-        dateOfBirth,
-        gender,
-        ssn,
-        occupation,
-});
-    res.json(newPatient);
+    try {
+        const newPatient = toNewPatient(req.body);
+
+        const addedPatient = patientService.addPatient(newPatient);
+
+        res.json(addedPatient);
+    } catch (e) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        res.status(400).send(e.message);
+    }
+
+//     const { name, dateOfBirth, gender, ssn, occupation } = req.body;
+//     const newPatient = patientService.addPatient({
+//         name,
+//         dateOfBirth,
+//         gender,
+//         ssn,
+//         occupation,
+// });
+//     res.json(newPatient);
 });
 
 export default router;
